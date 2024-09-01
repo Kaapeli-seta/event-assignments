5c23d48e544187acd9eb12a81c25f144dfb1be2d
Kaapo <142903354+Kaapeli-seta@users.noreply.github.com>
2024-09-01 16:59:20 +0300

    Update

--- t1/t1.js
+++ t1/t1.js
@@ -28,3 +28,56 @@ const todoList = [
 ];
 
 // add your code here
+
+function newCheckbox(todo) {
+  const input = document.createElement('input');
+  input.type = 'checkbox';
+  input.id = 'todo-' + todo.id;
+  input.checked = todo.completed;
+  const label = document.createElement('label');
+  label.htmlFor = 'todo-' + todo.id;
+  label.innerText = todo.task;
+  const remove = document.createElement('button');
+  remove.htmlFor = 'todo-' + todo.id;
+  remove.innerText = 'X';
+  const li = document.createElement('li');
+  li.append(input, label, remove);
+  lista.appendChild(li);
+  input.addEventListener('click', function () {
+    todo.completed = todo.completed ? false : true;
+    console.log(todoList);
+  });
+  remove.addEventListener('click', function () {
+    lista.removeChild(li);
+    todoList.splice(todoList.indexOf(todo), 1);
+    console.log('remove todo-' + todo.id);
+    console.log(todoList);
+  });
+}
+
+const lista = document.querySelector('#target');
+const addBtn = document.querySelector('.add-btn');
+const modal = document.querySelector('dialog');
+const submit = document.querySelector('form > button');
+const form = document.querySelector('form > input');
+
+addBtn.addEventListener('click', function () {
+  modal.showModal();
+});
+
+submit.addEventListener('click', function () {
+  modal.close();
+  const newValue = {
+    id: todoList.at(todoList.length - 1).id + 1,
+    task: form.value,
+    completed: false,
+  };
+  form.value = '';
+  todoList.push(newValue);
+  newCheckbox(newValue);
+  console.log(todoList);
+});
+
+for (const todo of todoList) {
+  newCheckbox(todo);
+}
